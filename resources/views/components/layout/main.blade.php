0de1f1d286cce08aef031cd96cf402fdc1ca357d
Chiara Cassarà <chiaracassara7@gmail.com>
2023-08-08 20:37:58 +0200

    nuova navbar

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite(["resources\css\app.css", "resources\css\app.css"])
+    @vite(["resources\css\app.css", "resources\js\app.js"])
+
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Barriecito&family=Mulish:ital,wght@1,300&family=Roboto:ital,wght@0,700;1,700&family=Satisfy&family=Shantell+Sans:wght@300;400&display=swap');
+    </style>
 
     <title>{{$pageName}}</title>
 </head>
@@ -15,7 +19,9 @@
     </header>
 
     <main>
-        {{$slot}}
+    <div style="background-image: url('https://thumbs.dreamstime.com/z/cart-background-made-carts-vector-illustration-36868023.jpg?w=768'); background-repeat: no-repeat; background-size: cover;" class="bg-image vh-100">
+            {{ $slot }}
+        </div>
     </main>
 
     <footer class="mt-auto">
--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,16 +1,34 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary shadow fixed-top">
+<nav class="navbar navbar-expand-lg bg-warning bg-gradient shadow fixed-top">
+
+  
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="/">
+      <img src="https://png.pngtree.com/png-vector/20220821/ourmid/pngtree-speed-arrow-fast-quick-icon-logo-design-png-image_6119232.png" alt="Logo" width="60">
+      <a class="navbar-brand" id='logo' href="#">Presto.it</a>
+    </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+    <div class="collapse navbar-collapse justify-content-start text-decoration-underline fw-semibold" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">Annunci</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Crea</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Lavora con noi</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Lingua
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Italiano</a></li>
+            <li><a class="dropdown-item" href="#">Inglese</a></li>
+            <li><a class="dropdown-item" href="#">Spagnolo</a></li>
+          </ul>
         </li>
       </ul>
     </div>
