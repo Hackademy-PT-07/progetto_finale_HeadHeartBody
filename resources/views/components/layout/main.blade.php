24e6565c2d3405f5903d623d53015801147790a1
Chiara Cassarà <chiaracassara7@gmail.com>
2023-08-08 20:50:37 +0200

    integrazione navbar

--- resources/views/components/layout/main.blade.php
+++ resources/views/components/layout/main.blade.php
@@ -4,7 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite(["resources\css\app.css", "resources\css\app.css"])
+    @vite(["resources\css\app.css", "resources\js\app.js"])
+
+    <style>
+    @import url('https://fonts.googleapis.com/css2?family=Barriecito&family=Mulish:ital,wght@1,300&family=Roboto:ital,wght@0,700;1,700&family=Satisfy&family=Shantell+Sans:wght@300;400&display=swap');
+    </style>
 
     <title>{{$pageName}}</title>
 </head>
