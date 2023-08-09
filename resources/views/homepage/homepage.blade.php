97862afd0a0546fb8ddaf678e2aee032545f1f0b
Massimiliano <maxattianesekr94@gmail.com>
2023-08-09 09:35:12 +0200

    Riordino del codice - completato fortify con il logout

--- resources/views/homepage/homepage.blade.php
+++ resources/views/homepage/homepage.blade.php
@@ -2,6 +2,13 @@
     <x-slot:pageName>Homepage</x-slot:pageName>
 
     <div class="pt-5 mt-5 text-center">
-        <h2>Max-Alex-Chiara-Francesco-Nunzia</h2>
+        <h2 class="pt-4 fw-bold">Presto.it</h2>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="text-end pt-5">
+                <a class="btn btn-primary" href="#">Inserisci annuncio</a>
+            </div>
+        </div>
     </div>
 </x-main>
