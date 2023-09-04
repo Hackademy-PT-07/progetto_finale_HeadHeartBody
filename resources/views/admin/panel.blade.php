3f5c24060a46ce67f872417096834aa544b73a7f
Massimiliano <maxattianesekr94@gmail.com>
2023-09-04 21:26:56 +0200

    unione progetto

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -2,8 +2,10 @@
     <x-slot:pageName>Pannello Admin</x-slot:pageName>
 
     <div class="row pt-5 mt-5">
-        <div class="col-6">
+        <h2 class="text-center">Pannello Admin</h2>
+        <div class="col-12 col-xl-6">
             <div class="row flex-column align-items-center">
+                <h3 class="pt-5">Categorie:</h3>
                 <div class="col-10 d-flex flex-column justify-content-center" style="height: 400px;">
                 <livewire:category-form/>
                 </div>
@@ -12,7 +14,8 @@
                 </div>
             </div>
         </div>
-        <div class="col-6">
+        <div class="col-12 col-xl-6 py-5 pt-xl-0">
+            <h3 class="pt-5">User:</h3>
             <div class="row flex-column align-items-center">
                 <div class="col-10" style="height: 400px;">
                     <livewire:user-form/>
