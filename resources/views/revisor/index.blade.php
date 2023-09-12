e986ea697c0e7ebdbec06546de1091ef4202f698
nunzia riglietti <rigliettinunzia1@gmail.com>
2023-09-12 02:37:19 +0200

    prova pull

--- resources/views/revisor/index.blade.php
+++ resources/views/revisor/index.blade.php
@@ -1,96 +1,54 @@
 <x-main>
+    <x-slot:pageName>{{ __('ui.announces') }}</x-slot:pageName>
 
-    <x-slot:pageName>{{ __('ui.revisor') }}</x-slot:pageName>
+    <x-search :category="$category" :searched="$searched" :order="$order"/>
 
-    <div class="container pt-5 mt-5">
-        @if($announce_to_check)
-        <section class="row">
-            <div class="text-start pt-4">
-                <a class="btn btn-warning buttonStyle text-end" href="{{route('announces.index')}}">{{ __('ui.back') }}</a>
-            </div>
-            <h2 class="text-center fw-bold fst-italic pb-4">{{$announce_to_check->title}}</h2>
-            <div class="col-0 col-xl-1"></div>
-            <div class="col-12 col-xl-10">
-                <div id="carouselExampleFade" class="carousel slide carousel-fade">
-                    <div class="carousel-inner">
-                        @if(count($announce_to_check->images))
-                        @foreach($announce_to_check->images as $image)
-                        <div class="carousel-item @if($image == $announce_to_check->images()->first()) active @endif">
-                            <img style="width: 400px; height: 450px;" src="{{Storage::url($image->path)}}" class="d-block w-100 mb-5" alt="Foto Annuncio">
-
-                            <span>Tags:</span> <br>
-                            @foreach($image->labels as $label)
-                            <span class="ps-4">{{$label}},</span>
-                            @endforeach
-                            <hr>
-                            <div class="d-flex justify-content-between">
-                                <span>Adulti: <span class="{{$image->adult}}"></span></span>
-                                <span>Satira: <span class="{{$image->spoof}}"></span></span>
-                                <span>Medicina: <span class="{{$image->medical}}"></span></span>
-                                <span>Violenza: <span class="{{$image->violence}}"></span></span>
-                                <span>Contenuto Ammiccante: <span class="{{$image->racy}}"></span></span>
-                            </div>
-                        </div>
-                        @endforeach
-                        @else
-                        <div class="carousel-item active">
-                            <img style="width: 400px; height: 450px;" src="{{asset('img/non-disponibile.gif')}}" class="d-block w-100" alt="Foto Annuncio">
-                        </div>
-                        @endif
-
-                    </div>
-                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Previous</span>
-                    </button>
-                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="visually-hidden">Next</span>
-                    </button>
+    <div class="container mt-4 justify-content-center py-3">
+        <div class="row">
+            <div class="container col-12 d-inline-flex mt-0">
+                <div class="col-3 text-center">
+                    <img class="immagine" src="https://png.pngtree.com/png-clipart/20220926/ourmid/pngtree-3d-red-portable-speaker-megaphone-announcement-and-promotion-png-image_6217784.png" alt="megafono">
                 </div>
-                <div class="col-0 col-xl-1"></div>
+                <div class="col-9 formTitle mt-3">
+                    <div class="text-center">
+                        <h2 class="d-inline">{{ __('ui.announces') }}</h2>
+                    </div>
+                </div>
             </div>
-        </section>
-        <section class="row py-5 mt-5">
-            <div class="col-1 col-xl-0"></div>
-            <div class="col-12 col-xl-7 text-center">
-                <h3 class="small fst-italic fs-2 pb-2">{{ __('ui.descAd') }}</h3>
-                <p class="fs-4">{{$announce_to_check->description}}</p>
-            </div>
-            <div class="col-12 col-xl-3 pt-5 pt-xl-0">
-                <span class="small fst-italic">{{ __('ui.priceAd') }}: </span>
-                <p class="text-center fw-bold border-bottom pb-2">{{$announce_to_check->price}}€</p>
-                <span class="small fst-italic">{{ __('ui.pubFrom') }}: </span>
-                <p class="text-center fw-bold border-bottom pb-2">{{$announce_to_check->user->name}} </p>
-                <span class="small fst-italic">{{ __('ui.pubOn') }}: </span>
-                <p class="text-center fw-bold border-bottom pb-2">{{$announce_to_check->created_at->format("d/m/Y")}}</p>
-            </div>
-            <div class="col-1 col-xl-0"></div>
-        </section>
 
-        <section class="row px-5">
-            <div class="col-12 d-flex justify-content-between pt-3 pb-5 px-5">
-                <form class="ps-3" action="{{route('revisor.accept_announce', ['announce'=>$announce_to_check])}}" method="POST">
-                    @csrf
-                    @method("PATCH")
-                    <button type="submit" class="btn btn-lg btn-success shadow">{{ __('ui.accept') }}</button>
-                </form>
 
-                <form class="pe-3" action="{{route('revisor.reject_announce', ['announce'=>$announce_to_check])}}" method="POST">
-                    @csrf
-                    @method("PATCH")
-                    <button type="submit" class="btn btn-lg btn-danger shadow">{{ __('ui.decline') }}</button>
-                </form>
+            <!-- Announces card -->
+            @if(count($announces))
+            @foreach($announces as $announce)
+            <div class="col-12 col-md-6 col-lg-4 md-5 pt-3 d-flex justify-content-center">
+                <figure class="snip1418 card">
+                    <img class="img-fluid" style="height: 240px" @foreach($announce->images as $img) src="{{Storage::url($img->path)}}" @endforeach alt="{{$announce->title}}" />
+                    <div class="add-to-cart">
+                        <i class="ion-android-add"></i>
+                        <span>{{ __('ui.clickDetails') }}</span>
+                    </div>
+                    <figcaption class="snip1418Body">
+                        <h3 class="text-center text-decoration-underline p-2 purple card" id="title">{{ $announce->title }}</h3>
+
+                        <p class="card-text text-warning fw-semibold mb-2 text-end fs-6">{{__('ui.category_'.$announce->category_id)}}</p>
+
+                        <p class="hidden">{{ $announce->description }}</p>
+
+                        <div class="price text-center"><span>€{{ $announce->price }}</span></div>
+
+                        <p class="card-footer fst-italic"> {{ __('ui.createAt') }}: {{ $announce->created_at->format("d/m/Y") }} </p>
+                    </figcaption>
+
+                    <a href="{{route('announces.show', $announce->id) }}"></a>
+                </figure>
+                </div>
+            @endforeach
+            {{$announces->links()}}
+                </div>
             </div>
-        </section>
+            @endif
 
-        @else
-        <section class="row pt-5 mt-5">
-            <div class="col-12">
-                <p class="text-center">{{ __('ui.noRevAd') }} <a href="{{route('revisor.revised')}}" class="btn btn-secondary">{{ __('ui.adRevised') }}</a></p>
-            </div>
-        </section>
-        @endif
-    </div>
+            <!-- Announces card end -->
 
+        </div>
 </x-main>
